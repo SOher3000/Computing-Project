1732c7dd2e7c22ecba4624838249a578956a404b
SOher3000 <149064585+SOher3000@users.noreply.github.com>
2024-06-19 22:31:23 +0300

    Some refactoring

--- login.php
+++ login.php
@@ -1,53 +1,92 @@
 <?php
-	include_once('db/db.php');
-	include_once('header.html');
-	include_once('reducedMenu.php');
-	
-	$error = '';
-	
-	if(isset($_POST['submitted'])) {
-	$Username=mysqli_real_escape_string($db, trim($_POST['Username']));
-	$password=mysqli_real_escape_string($db, trim($_POST['password']));
-	
-	if(!empty($Username) && !empty($password)){
-	$result = mysqli_query($db,"select * from users where name ='$Username' and password = MD5('$password')");
-	if ($result->num_rows != 1) {
-	$error = 'Query filed - Please try again';
-	} else {
-		$userData = mysqli_fetch_array($result);
-	session_start();
-	$_SESSION['userid'] = $userData["userid"];
-	$_SESSION['user'] = $Username;
-	header('Location: index.php');
-	exit();
-	}
-	} else {
-	$error = 'Please fill both';
-	}
-	
-	}
-	if(!empty($error)){
-	echo "<div class='error' >$error</div>";
-	}
-	
-	
-	/*
-	
-	USER ACCOUNT CREATION
-	
-	IF NEW USER ACCOUNT NEEDED ACCESS usrAccCrtn.php TO REGISTER NEW USER
-	
-	*/
-	
-	?>
-	
-	<div id='content' >
-		<h3>Login</h3>
-		<form action='' method='POST' >
-		Username&nbsp;<input type='text' name='Username'/><br/>
-		Password&nbsp;<input type='password' name='password' /><br/>
-		<input type='hidden' name='submitted' value='true' />
-		<input type='submit' value='Login'/>
-		</form>
-	</div>
-	
+include_once 'db/db.php';
+include_once 'header.html';
+include_once 'reducedMenu.php';
+
+class LoginHandler
+{
+    private $db;
+    private $error;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+        $this->error = '';
+    }
+
+    public function handleLogin()
+    {
+        if (isset($_POST['submitted'])) {
+            $username = $this->sanitizeInput($_POST['Username']);
+            $password = $this->sanitizeInput($_POST['password']);
+
+            if (!empty($username) && !empty($password)) {
+                if ($this->authenticateUser($username, $password)) {
+                    $this->startUserSession($username);
+                    header('Location: index.php');
+                    exit();
+                } else {
+                    $this->error = 'Query failed - Please try again';
+                }
+            } else {
+                $this->error = 'Please fill both';
+            }
+        }
+    }
+
+    private function sanitizeInput($input)
+    {
+        return htmlspecialchars(trim($input));
+    }
+
+    private function authenticateUser($username, $password)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE name = :username AND password = MD5(:password)");
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+
+        return $stmt->rowCount() === 1;
+    }
+
+    private function startUserSession($username)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE name = :username");
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        $userData = $stmt->fetch();
+
+        session_start();
+        $_SESSION['userid'] = $userData["userid"];
+        $_SESSION['user'] = $username;
+    }
+
+    public function displayError()
+    {
+        if (!empty($this->error)) {
+            echo "<div class='error'>{$this->error}</div>";
+        }
+    }
+}
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=your_database_name', 'your_username', 'your_password');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $loginHandler = new LoginHandler($db);
+    $loginHandler->handleLogin();
+    $loginHandler->displayError();
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+<div id='content'>
+    <h3>Login</h3>
+    <form action='' method='POST'>
+        Username&nbsp;<input type='text' name='Username'/><br/>
+        Password&nbsp;<input type='password' name='password'/><br/>
+        <input type='hidden' name='submitted' value='true'/>
+        <input type='submit' value='Login'/>
+    </form>
+</div>
